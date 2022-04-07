62cbf62a50989d8719511935396fb39551df56db
Borni Ahmed Rami <borniahmedrami@gmail.com>
2022-04-07 03:49:04 +0100

    [Update] Add Routing to Project , Add Navbar , and Logo

---
+++ src/routes/navbar/navbar.component.jsx
@@ -0,0 +1,20 @@
+import {Link, Outlet} from 'react-router-dom';
+import {ReactComponent as WissLogo} from '../../assets/crown.svg';
+import './navbar.styles.scss';
+const Navbar = () => {
+    return (
+        <>
+            <nav className="navigation">
+                <Link className="logo-container" to="/">
+                    <WissLogo/>
+                </Link>
+                <div className="nav-links-container">
+                    <Link className="nav-link" to='/shop'>SHOP</Link>
+                </div>
+            </nav>
+            <Outlet/>
+        </>
+    );
+}
+
+export default Navbar;
